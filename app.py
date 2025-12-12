0a9662d286b04e031405c5f25f4c6bdcece9d1bd
24caratgoldenlabububobatea <jonathanrasmussen2020@gmail.com>
2025-12-12 13:52:22 +0000

    I didn't know i had to connect it to the website we made last time so i'm doing that now

--- app.py
+++ app.py
@@ -1,10 +1,90 @@
 from flask import Flask, render_template, redirect
 import mysql.connector
 from forms import RegisterForm, LoginForm
+import datetime
 
 app = Flask(__name__)
 app.secret_key = 'hemmlig_nokkel'
 
+
+# ---------------------------
+#  ADDED FROM THE FIRST FILE
+# ---------------------------
+
+regular_meals = [
+    {"name": "Pizza med ost", "image": "/bilder/pizza.png"},
+    {"name": "Kyllingnuggets med microplast", "image": "/bilder/nugget.png"},
+    {"name": "Spaghetti med spaghet", "image": "/bilder/spagetti.png"}
+]
+
+weekly_specials = [
+    [   # Monday
+        {"name": "Taco som er ok - 2kr", "image": "bilder/taco.png"},
+        {"name": "Chili uten chill 34kr", "image": "bilder/chili.png"},
+        {"name": "Burritos med kristene bønner 22kr", "image": "bilder/taco.png"}
+    ],
+    [   # Tuesday
+        {"name": "Hamburger fra hamburg 333kr", "image": "bilder/hamburger.png"},
+        {"name": "Pølse med brød 123kr", "image": "bilder/polse.png"},
+        {"name": "Pommes frites med ekstra pommes 12kr", "image": "bilder/pommes.png"}
+    ],
+    [   # Wednesday
+        {"name": "Fiskepinner uten pinne 30kr", "image": "bilder/fiskepinne.jpg"},
+        {"name": "Macbok and Cheese 394kr", "image": "bilder/macncheese.png"},
+        {"name": "Salat med salsa danser 223kr", "image": "bilder/salat.png"}
+    ],
+    [   # Thursday
+        {"name": "Kjøttkake med stor kake 11kr", "image": "bilder/kjottkake.png"},
+        {"name": "Potetmos moset potet 23kr", "image": "bilder/potetmos.png"},
+        {"name": "Mais er kult 94kr", "image": "bilder/mais.png"}
+    ],
+    [   # Friday
+        {"name": "Lasagne med lala 94kr", "image": "bilder/lasagne.png"},
+        {"name": "Kebab med bob habab 233kr", "image": "bilder/kebab.png"},
+        {"name": "Cæsarsalat med ekstra cæsar 999kr", "image": "bilder/cæsar.png"}
+    ]
+]
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/meny")
+def meny():
+    today_index = datetime.datetime.now().weekday()
+
+    if today_index < 5:
+        meals_today = regular_meals + weekly_specials[today_index]
+        day_name = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag"][today_index]
+    else:
+        meals_today = [
+            {"name": "Ingen måltid i helgen!", "image": "/bilder/no_food.png"}
+        ]
+        day_name = "Helg"
+
+    return render_template("meny.html", meals=meals_today, day=day_name)
+
+
+@app.route("/varer")
+def varer():
+    day_name = "Faste varer"
+    return render_template("varer.html", meals=regular_meals, day=day_name)
+
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
+# ---------------------------
+# END OF ADDED PART
+# ---------------------------
+
+
+# ---------------------------
+# DATABASE FUNCTIONS
+# ---------------------------
 
 def get_conn():
     return mysql.connector.connect(
@@ -14,9 +94,14 @@ def get_conn():
         database="eksempel_db"
     )
 
+
+# ---------------------------
+# REGISTER
+# ---------------------------
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    form = RegisterForm()  
+    form = RegisterForm()
 
     if form.validate_on_submit():
         navn = form.name.data
@@ -35,14 +120,17 @@ def register():
 
         return redirect("/login")
 
-    
     return render_template("register.html", form=form)
+
+
+# ---------------------------
+# LOGIN
+# ---------------------------
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()  
+    form = LoginForm()
 
-    # Sjekk om skjemaet er sendt inn og gyldig
     if form.validate_on_submit():
         brukernavn = form.username.data
         passord = form.password.data
@@ -53,16 +141,13 @@ def login():
             "SELECT navn FROM kunder WHERE brukernavn=%s AND passord=%s",
             (brukernavn, passord)
         )
-        user = cur.fetchone() 
+        user = cur.fetchone()
         cur.close()
         conn.close()
 
-        
         if user:
             return render_template("welcome.html", name=user[0])
         else:
             form.username.errors.append("Feil brukernavn eller passord")
 
     return render_template("login.html", form=form)
-
-
